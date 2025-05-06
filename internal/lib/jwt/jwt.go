1bf2bc34bfd2de4656ed37d0ca7a804eb954859d
FozardNB <fzrd2000@gmail.com>
2025-05-06 19:00:41 +0300

    added jwt generator

--- internal/lib/jwt/jwt.go
+++ internal/lib/jwt/jwt.go
@@ -9,13 +9,15 @@ import (
 )
 
 func NewToken(GUID string, duration time.Duration) (string, error) {
-	token := jwt.New(jwt.SigningMethodES512)
+	token := jwt.New(jwt.SigningMethodHS512)
 
 	claims := token.Claims.(jwt.MapClaims)
 	claims["guid"] = GUID
 	claims["exp"] = time.Now().Add(duration).Unix()
 
-	tokenString, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+	secret := os.Getenv("JWT_SECRET")
+
+	tokenString, err := token.SignedString([]byte(secret))
 	if err != nil {
 		return "", fmt.Errorf("failed to generate JWT:%w", err)
 	}
